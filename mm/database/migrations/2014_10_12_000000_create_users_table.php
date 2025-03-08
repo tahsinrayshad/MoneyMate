bad5d8b56791e58d297bd9bb4dadbc4f5cbe5b9c
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-08 22:53:28 +0600

    Add Google authentication using Socialite; update user migration and routes

--- mm/database/migrations/2014_10_12_000000_create_users_table.php
+++ mm/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('google_id')->nullable();
             $table->string('fullname');
             $table->rememberToken();
             $table->timestamps();
--- mm/routes/web.php
+++ mm/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleAuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('google', [GoogleAuthController::class, 'redirect'])->name('google-auth');
+Route::get('google/call-back', [GoogleAuthController::class, 'callbackGoogle']);
